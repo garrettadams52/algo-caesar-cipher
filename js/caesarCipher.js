6184da2ec163c630781f479afd8fbee6ad41b182
Garrett Adams <garrett.adams52@gmail.com>
2022-05-31 16:51:34 -0400

    finished js

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,3 +1,23 @@
-exports.caesarCipher = function() {
 
-};
+
+exports.caesarCipher = function(str, num) {
+    str = str.split('')
+    re = /^[a-zA-Z]/
+    ans = []
+    let j, temp;
+
+    for(let i = 0; i<str.length; i++){
+        if(re.test(str[i])){
+            j = str[i]===str[i].toUpperCase() ? 65 : 97
+            temp = (((str[i].charCodeAt(0))-j+num)%26)
+            temp = temp<0 ? temp+=(j+26) : temp+=j
+            ans.push(String.fromCharCode(temp))
+        }
+        else{
+            ans.push(str[i])
+        }
+
+    }
+    return ans.join('')
+}
+
