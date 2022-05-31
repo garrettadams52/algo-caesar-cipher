4eac62af2ac441ce23337fcc2f72e211e70260f5
Garrett Adams <garrett.adams52@gmail.com>
2022-05-31 15:55:10 -0400

    finished python

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,17 @@
+import re
+
 def caesar_cipher(string, shift_amount):
+    string = list(string)
+    let = "^[a-zA-Z]"
+    ans = []
+
+    for i,x in enumerate(string):
+        if re.match(let,x):
+            j = 97 if x.islower() else 65
+            temp = ((ord(x)-j+shift_amount)%26)+j
+            x = chr(temp)
+        ans.append(x)
+
+    return ''.join(ans)
+
+
